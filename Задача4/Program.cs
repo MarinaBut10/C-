58c60a6a0fde72a63c1819fac99ffb3fade136bd
Marina <lemur4icc@mail.ru>
2023-02-02 23:54:11 +0300

    задача4, семинар1

---
+++ Задача4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите число x: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число y: ");
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число z: ");
+int z = int.Parse(Console.ReadLine());
+
+if (x > y && x > z) Console.WriteLine($"Макс.число {x}");
+if (y > x && y > z) Console.WriteLine($"Макс.число {y}");
+if (z > y && z > x) Console.WriteLine($"Макс.число {z}");
+if (x == y && y== z && x == z) Console.WriteLine("Числа равны друг другу");
